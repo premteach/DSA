aefd623c60ca1435f5c60fa23b52abbe9060aead
Prem Chandra Gupta <149512766+premteach@users.noreply.github.com>
2026-05-02 13:09:30 +0530

    Time: 38 ms (68.68%) | Memory: 98.7 MB (45.09%) - LeetSync

---
+++ 456-132-pattern/132-pattern.java
@@ -0,0 +1,22 @@
+class Solution {
+    public boolean find132pattern(int[] nums) {
+        int n = nums.length;
+        int third = Integer.MIN_VALUE;
+
+        Stack<Integer> stack = new Stack<>();
+
+        for(int i = n - 1; i >= 0; i--) {
+            if(nums[i] < third) {
+                return true;
+            }
+
+            while(!stack.isEmpty() && stack.peek() < nums[i]) {
+                third = stack.pop();
+            }
+
+            stack.push(nums[i]);
+        }
+
+        return false;
+    }
+}
